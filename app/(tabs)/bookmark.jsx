2f0d3b492945a2cdfc63cabc93e67f72909781a5
Subham Kumar <subhamk1978@gmail.com>
2024-08-22 20:51:26 +0530

    video upload feature

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Bookmark = () => {
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
+    <SafeAreaView className="h-full bg-primary flex flex-col justify-center">
+      <Text className="text-center text-white font-psemibold text-2xl">This Feature is coming <Text className="text-secondary-200" >soon</Text>!!!</Text>
+    </SafeAreaView>
   )
 }
 
